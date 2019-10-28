3a27700e2d0206a08c3e32dfb36a8b7df64ae8b5
Jryke <rykerson@gmail.com>
2019-10-28 15:04:53 -0400

    conditional used to toggle add/remove like

--- controllers/patchUser.js
+++ controllers/patchUser.js
@@ -5,12 +5,23 @@ module.exports = (req, res) => {
 	let userInfo = jwt.verify(req.params.token, process.env.SECRET)
 	User.findById(userInfo._id)
 	.then(userData => {
-		userData.likes.push(req.body.likes)
-		User.findByIdAndUpdate(userInfo._id, userData)
-		.then(data => {
-			data.likes = data.likes.push(req.body.likes)
-			res.send(data)
-		})
-		.catch(err => {res.send(err)})
+		if (userData.likes.includes(req.body.likes)) {
+			let index = userData.likes.indexOf(req.body.likes)
+			userData.likes.splice(index, 1)
+			User.findByIdAndUpdate(userInfo._id, userData, {useFindAndModify: false})
+			.then(data => {
+				data.likes.splice(index, 1)
+				res.send(data)
+			})
+			.catch(err => {res.send(err)})
+		} else {
+			userData.likes.push(req.body.likes)
+			User.findByIdAndUpdate(userInfo._id, userData, {useFindAndModify: false})
+			.then(data => {
+				data.likes = data.likes.push(req.body.likes)
+				res.send(data)
+			})
+			.catch(err => {res.send(err)})
+		}
 	})
 }
