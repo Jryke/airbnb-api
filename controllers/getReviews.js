985c6fa6c5274b9f76947cacdde4a49328a907d0
Jryke <rykerson@gmail.com>
2019-10-30 09:59:27 -0400

    sending response data

--- controllers/getReviews.js
+++ controllers/getReviews.js
@@ -4,6 +4,8 @@ module.exports = (req, res) => {
 	Review.find({place: req.params.id})
 	.populate('User')
 	.populate('Place')
-	.then(data => res.send(data))
+	.then(data => {
+		res.send(data)
+	})
 	.catch(err => res.send(err))
 }
