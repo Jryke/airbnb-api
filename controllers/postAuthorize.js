477f3174549a2937a39930dd63fc528711455b72
Jryke <rykerson@gmail.com>
2019-10-30 10:02:02 -0400

    finding user by id

--- controllers/postAuthorize.js
+++ controllers/postAuthorize.js
@@ -1,10 +1,9 @@
-// const Authorize = require('../models/authorize')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
 	let userInfo = jwt.verify(req.body.token, process.env.SECRET)
-	User.findById(userInfo._id).select('-__v -_id').then(user => {
+	User.findById(userInfo._id).select('-__v').then(user => {
 		res.send(user)
 	})
 }
